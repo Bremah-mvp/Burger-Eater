78c7110dbb36bf336e74a757697a3a6f9039eca2
jackie <mvpbremah@yahoo.com>
2020-09-15 23:16:42 -0400

    commit

---
+++ config/orm.js
@@ -0,0 +1,13 @@
+// import MYSQL connection
+var connection = require("../config/connection.js");
+
+// build queries
+function printQuestionMarks(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return err.toString();
+}
+
+function 
