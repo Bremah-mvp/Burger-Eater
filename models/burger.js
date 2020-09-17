700876c39c293c661d3f1674ab3f46dd9600075c
jackie <mvpbremah@yahoo.com>
2020-09-17 19:45:46 -0400

    commit

---
+++ models/burger.js
@@ -0,0 +1,12 @@
+// import the ORM
+var orm = require("../config/orm.js");
+
+// create burger
+var burger = {
+
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        })
+    }
+}
