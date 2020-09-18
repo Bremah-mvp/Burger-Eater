7f750ced357398f40b63e45453f58ff87b0d8aae
jackie <mvpbremah@yahoo.com>
2020-09-17 21:06:41 -0400

    commit

--- server.js
+++ server.js
@@ -3,3 +3,18 @@ var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
 
+var PORT = process.env.PORT || 3000;
+
+// express app
+var app = express();
+app.use(express.static(_dirname + '/public'));
+
+// bodyparsers
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type'}));
+app.use(bodyParser.text({ type: 'text/html' }));
+
+// override with POST having ?_method= DELETE or PUT
+app.use(methodOverride('_method'))
