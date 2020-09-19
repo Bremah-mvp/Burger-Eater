eb573d5ed4504a24bc7f36a5653ee0d4031d846d
jackie <mvpbremah@yahoo.com>
2020-09-18 21:06:14 -0400

    commit

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,37 +1,45 @@
-// dependencies
-
-var express = require("express");
+// Required depedencies
+var express = require('express');
 var router = express.Router();
-var burger = require("../models/burger.js");
+var burger = require('../models/burger.js');
 
-// routes
-router.get("/", function(req, res) {
-    res.redirect("/index");
+// Create routes
+
+// Index redirect
+router.get('/', function(req, res) {
+	res.redirect('/index');
 });
 
-router.get("/index", function(req, res) {
-    burger.selectAll(function(data) {
-        var hbsObject = {burgers: data};
-        res.render("index", hbsObject);
-    });
+
+// selectAll
+router.get('/index', function(req, res) {
+	burger.selectAll(function(data) {
+		var hbsObject = {burgers: data};
+		res.render('index', hbsObject);
+	});
 });
 
-// insert one burger
-router.post("/burgers/insertOne", function(req, res) {
-    burger.insertOne(["burger_name", "devoured"], [req.body.name, false], function() {
-        res.redirect("/index");
-    });
+
+
+// insertOne
+router.post('/burgers/insertOne', function(req, res) {
+	burger.insertOne(['burger_name', 'devoured'], [req.body.name, false], function() {
+		res.redirect('/index');
+	});
 });
 
-// update one
-router.put("/burgers/updateOne/:id", function(req, res) {
-    var condition = "id =" + res.params.id;
-    console.log("condition", condition);
 
-    burger.updateOne({devoured: res.body.devoured}, condition, function() {
-        res.redirect("/index");
 
-    });
+// updateOne
+router.put('/burgers/updateOne/:id', function(req, res) {
+	var condition = 'id = ' + req.params.id;
+	console.log('condition', condition);
+
+	burger.updateOne({devoured: req.body.devoured}, condition, function() {
+		res.redirect('/index');
+	});
 });
 
+
+// Export 
 module.exports = router;
