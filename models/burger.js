d70205b7a8e3b0b43ddd8db684a9e37632cdc956
jackie <mvpbremah@yahoo.com>
2020-09-18 20:57:07 -0400

    commit

--- models/burger.js
+++ models/burger.js
@@ -1,26 +1,31 @@
-// import the ORM
+// Import the ORM
 var orm = require("../config/orm.js");
 
-// create burger
+// create burger 
 var burger = {
+	
+	selectAll: function(cb) {
+		orm.selectAll('burgers', function(res) {
+			cb(res);
+		});
+	},
+	
 
-    selectAll: function(cb) {
-        orm.selectAll("burgers", function(res) {
-            cb(res);
-        });
-    },
 
-    insertOne: function(cols, vals, cb) {
-        orm.insertOne("burgers", cols, vals, function(res) {
-            cb(res);
-        });
-    },
+	insertOne: function(cols, vals, cb) {
+		orm.insertOne('burgers', cols, vals, function(res) {
+			cb(res);
+		});
+	},
+	
 
-    updateOne: function(objColVals, condition, cb) {
-        orm.updateOne("burgers", objColVals, condition, function(res) {
-            cb(res);
-        });
-    }
+
+	updateOne: function(objColVals, condition, cb) {
+		orm.updateOne('burgers', objColVals, condition, function(res) {
+			cb(res);
+		});
+	}
 };
 
+// Export 
 module.exports = burger;
