52e3e088761265cbac2bb5cccde6af1b3c9526d0
jackie <mvpbremah@yahoo.com>
2020-09-19 16:19:06 -0400

    commit

--- config/connection.js
+++ config/connection.js
@@ -7,10 +7,10 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 	connection = mysql.createConnection({
-		host: process.env.DB_HOST,
+		host: "localhost",
 		port: 3306,
-		user: process.env.DB_USER,
-		password: process.env.DB_PASSWORD,
+		user: "root",
+		password: "Bremen12345$",
 		database: 'burgers_db'
 	});
 }
