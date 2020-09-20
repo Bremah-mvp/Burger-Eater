e534f833f8c70694a40a3978467dbfc9d5950c99
jackie <mvpbremah@yahoo.com>
2020-09-20 10:35:00 -0400

    commit

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,2 @@
-INSERT INTO
-    burgers(burger_name, devoured)
-VALUES
-    ("Gourdon-Hamsey", FALSE),
-    ("The Silentil Night", FALSE),
-    ("Poutine on the Ritz", FALSE),
-    ("Poblano Picasso", FALSE),
-    ("Eggers Can't Be Cheesers", FALSE)
-    ("New Bacon-ings", true),
-    ("The Foot Feta-ish", true),
-    ("Rest in Peas", true),
-    ("Fig-eta Bout It", true)
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Bacon Double Cheeseburger", 0), ("Black Bean Burger with Lettuce, Tomato and Avocado", 0), ("Chicken Burger", 1)
